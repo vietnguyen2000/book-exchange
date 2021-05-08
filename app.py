5689614766b7edb5ee8d3fbb31f9f48d75b7a8da
vietnguyen2000 <viet.nguyen.2000@hcmut.edu.vn>
2021-05-08 14:39:34 +0700

    init first route BE

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 from flask import Flask
-from flaskext.mysql import MySQL
-from flaskext.mysql import MySQL
-mysql = MySQL()
-mysql.init_app(app)
+from flask_mysqldb import MySQL
+from collections import OrderedDict
+import json
+
 app = Flask(__name__)
 
 
-@app.route('/books' , method = ['POST'])
+app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql6411044'
+app.config['MYSQL_PASSWORD'] = 'KQwIHnkkbj'
+app.config['MYSQL_DB'] = 'sql6411044'
+
+mysql = MySQL(app)
+
+@app.route('/books' , methods = ['POST'])
 def getAllBooks():
-    return 'Hello, World!'
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM book NATURAL JOIN user')
+    mysql.connection.commit()
+    data = cur.fetchall()
+    cur.close()
+    response = []
+    for row in data:
+        response.append({})
+        response[-1]['id'] = row[0]
+        response[-1]['title'] = row[1]
+        response[-1]['description'] = row[2]
+        response[-1]['imageURL'] = row[4]
+        response[-1]['user'] = {}
+        response[-1]['user']['id'] = row[3]
+        response[-1]['user']['username'] = row[5]
+        response[-1]['user']['name'] = row[7]
+
+    # print(json.dumps(data))
+    return json.dumps(response)
+
+
+
+app.run(host='localhost',port = 5000, debug= True)
